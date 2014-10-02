999d502f2de51db24b3800a0919bab3f840a4e44
Bohdan Chechin <bohdan.chechin@live.com>
2014-10-02 19:17:57 +0300

    initial for new server

--- app/models/Place.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Place extends Eloquent {
-	protected $table = 'places';
-	protected $guarded = array();
-
-	public function scopePopular($query) {
-		return $query->orderBy('votes', 'DESC');
-	}
-}
--- app/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-Route::get('/', function()
-{
-	return View::make('index');
-});
-Route::get('/places/popular', 'PlacesController@popular');
-Route::resource('/places', 'PlacesController');
-Route::put('/places/{id}/vote', 'PlacesController@vote');
