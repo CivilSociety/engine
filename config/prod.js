bf59ee792593ffba24b97429ad0ce66b0b9ff0e6
Bohdan Chechin <bohdan.chechin@live.com>
2015-01-22 21:35:59 +0200

    fix config

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,22 @@ module.exports = {
 		connectionString: process.env.MONGO_CONNECTION
 	},
 	publicConstants: {
-		facebookAppId: process.env.FACEBOOK_APP_ID
+		facebookAppId: process.env.FACEBOOK_APP_ID,
+		title: 'Мой Орёл',
+		url: 'http://localhost:3000/',
+		apiUrl: 'http://localhost:3000/',
+		lon: 36.06863021850586,
+		lat: 52.96766504873649,
+		zoom: 13
+	},
+	vk: {
+		clientID: '4558998',
+		clientSecret: 'WmwCGKf0RrfMhknFGeWk',
+		callbackURL: 'http://localhost:3000/auth/vkontakte/callback'
+	},
+	fb: {
+		clientID: '',
+		clientSecret: '',
+		callbackURL: ''
 	}
 };
